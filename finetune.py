2a3366606306d6967123aba2bd71d645d9280a9c
一念执着 <1830484565@qq.com>
2024-01-02 11:43:50 +0800

    update finetune.py

--- finetune.py
+++ finetune.py
@@ -123,7 +123,7 @@ def train(
 
     model = AutoModelForCausalLM.from_pretrained(
         base_model,
-        load_in_8bit=False,
+        load_in_8bit=True,
         torch_dtype=torch.float16,
         device_map=device_map,
     )
